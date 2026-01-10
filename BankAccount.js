7e06f9235562d175c6f2081f2083aaca2c58da1a
shagain <shagainsen3883@gmail.com>
2026-01-11 05:48:13 +1100

    Creating the BankAccoint basic js file so that it can handle the basic things like deposit, withdraw and trasnfering the money

---
+++ BankAccount.js
@@ -0,0 +1,21 @@
+class BankAccount {
+  constructor(AccountNumber, AccountHolderName, Balance) {
+    this.AccountNumber = AccountHolderName;
+    this.AccountHolderName = AccountHolderName;
+    this.Balance = Balance;
+  }
+
+  deposite() {
+
+  }
+
+  Withdraw() {
+
+  }
+
+  Trasaction() {
+
+  }
+
+
+}
