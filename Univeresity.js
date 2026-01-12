ae4bc6e8d7a3e2d191f12e1d1deffc7433d51f88
shagain <shagainsen3883@gmail.com>
2026-01-12 23:02:33 +1100

    updating the logioc of remove and adding the department

--- Univeresity.js
+++ Univeresity.js
@@ -15,6 +15,7 @@ class Department {
   }
 }
 
+
 class University {
   constructor(name, departments) {
     this.name = name;
@@ -25,49 +26,70 @@ class University {
     console.log(`University Name : ${this.name} `);
   }
 
-  addDepartment() {
-
-  }
-
-  removeDepartment(id) {
-
-    for (const dep of this.departments) {
-      if (dep.DepID === id) {
-        for (const dep of this.departments) {
-
-          let index = 1;
-          if (dep.DepID === id) {
-            console.log(`Department name: ${dep.DepName} with Department ID : ${dep.DepID} has been deleted succesfully`);
-            departmentList.splice(index, 1);
-
-            break;
-          }
-          index++;
-        }
-
-        console.log(`-- New list of department --`);
-
-        for (const UpdateDep of this.departments) {
-          UpdateDep.display();
-        }
-
-      }
-      else {
-        console.log(`${id} department ID not found`);
-      }
-    }
-
-
-
-  }
-
-
   displayAllDepartment() {
     for (let dep of this.departments) {
       dep.display();
     }
   }
 
+  addDepartment(name, id) {
+
+    // let duplicate = false;
+
+    for (const dep of this.departments) {
+      if (dep.DepID === id) {
+        console.log(`${id} department ID Already exists`);
+        duplicate = true;
+
+        return;
+        // break;
+      }
+    }
+
+    // if (duplicate == false) {
+    this.departments.push(new Department(name, id));
+    console.log(` Department ID ${id} id succesfully add to the University`);
+
+    console.log(`-- New list of department --`);
+    this.displayAllDepartment();
+
+    // for (const UpdateDep of this.departments) {
+    //   UpdateDep.display();
+    // }
+    // }
+  }
+
+  removeDepartment(id) {
+
+    for (const dep of this.departments) {
+      if (dep.DepID === id) {
+        console.log(`${id} department ID not found`);
+
+        return;
+      }
+
+      for (const dep of this.departments) {
+
+        let index = 1;
+        if (dep.DepID === id) {
+          console.log(`Department name: ${dep.DepName} with Department ID : ${dep.DepID} has been deleted succesfully`);
+          departmentList.splice(index, 1);
+
+          break;
+        }
+        index++;
+      }
+
+      console.log(`-- New list of department --`);
+
+      // for (const UpdateDep of this.departments) {
+      //   UpdateDep.display();
+      // }
+      this.displayAllDepartment();
+
+    }
+
+  }
 }
 
 
@@ -90,3 +112,4 @@ departmentList.push(Dep4);
 // Uni.displayAllDepartment();
 
 Uni.removeDepartment("D-001");
+Uni.addDepartment("Software Department", "D-006")
