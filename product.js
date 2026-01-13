11e28ad2592d4c666a3f917b266e5527c8e94a44
shagain <shagainsen3883@gmail.com>
2026-01-13 23:14:47 +1100

    adding contructor and a total method into the product class

---
+++ product.js
@@ -0,0 +1,20 @@
+// Write a JavaScript program that creates a class called Product
+//  with properties for product ID, name, and price. Include a method
+//   to calculate the total price by multiplying the price by the quantity
+//   . Create a subclass called PersonalCareProduct that inherits from 
+//   the Product class and adds an additional property for the discount 
+//   %. Override the total price calculation method to include the 
+//   discount%. Create an instance of the PersonalCareProduct class and
+//    calculate its total price.
+
+class Product {
+  constructor(productID, name, price) {
+    this.productID = productID;
+    this.name = name;
+    this.price = price;
+  }
+  totalPrice(quantity) {
+    let Fprice = this.price * quantity;
+    return Fprice;
+  }
+}
