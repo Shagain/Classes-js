f3b99d7709ee20b035dd2f2158b4d8b5058945e3
shagain <shagainsen3883@gmail.com>
2026-01-14 22:53:33 +1100

    adding a basic class thing in the aminal class

---
+++ Animal.js
@@ -0,0 +1,17 @@
+// Write a JavaScript program that creates a class called 'Animal' with properties
+//  for species and sound. Include a method to make the animal's sound. Create a 
+//  subclass called 'Dog' that inherits from the 'Animal' class and adds an additional 
+//  property for color. Override the make sound method to include the dog's color.
+//   Create an instance of the 'Dog' class and make it make its sound.
+
+class Animal {
+  constructor(species, sound) {
+    this.species = species;
+    this.sound = sound;
+  }
+
+  dispaly() {
+    console.log(` Species : ${this.species} makes make sound ${sound}`);
+  }
+
+}
